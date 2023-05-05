7f4414c44c1e7fb1267a3ad43900f2dc235942f2
Ernani <ernani.costa@vai.com.vc>
2023-05-05 09:17:06 -0300

    Add tests

---
+++ test_calculadora_de_rotas.py.py
@@ -0,0 +1,29 @@
+from calculadora_de_rotas import Rotas, Rota
+
+
+def test_rotas():
+    rotas = Rotas()
+
+    # Adiciona as rotas
+    rotas.adicionar_rota('A', 'B', 10)
+    rotas.adicionar_rota('B', 'C', 15)
+    rotas.adicionar_rota('A', 'C', 20)
+    rotas.adicionar_rota('C', 'D', 30)
+    rotas.adicionar_rota('C', 'E', 35)
+
+    # Cria a classe Rota e adiciona as rotas
+    rota = Rota()
+    rota.adicionar_rota('A', 'B', 10)
+    rota.adicionar_rota('B', 'C', 15)
+    rota.adicionar_rota('A', 'C', 20)
+    rota.adicionar_rota('C', 'D', 30)
+    rota.adicionar_rota('C', 'E', 35)
+
+    # Testa a função valor_rota com algumas rotas
+    assert rota.valor_rota(['A', 'B', 'C']) == 25
+    assert rota.valor_rota(['A', 'C', 'E', 'B']) is None
+    assert rota.valor_rota(['A', 'B', 'D']) is None
+
+if __name__ == '__main__':
+    test_rotas()
+    print('Todos os testes passaram.')
